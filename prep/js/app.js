f80491c510411b8e5445ae135b3b3a4e3fee20ab
unknown <ilubkin@unm.edu>
2021-07-31 14:15:54 -0400

    finish item adding form and begin adding JS functionality

--- prep/js/app.js
+++ prep/js/app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//The below functions handle the interface with Firebase
 let database = firebase.database(); //global variable for root of firebase database
 
 async function updateHelloWorldLocal() {
@@ -19,3 +20,39 @@ async function updateHelloWorldLocal() {
 
 //Primary goal: make an interface to add and edit items with ingredients and full set of information.
 //After this first step, extensive pseudocode and planning should be completed
+
+//DOM functions
+/*  Creation Date: 7/31/2021
+    Author: Ian Lubkin
+    Description: Add a row with a text and number input field to a div 
+    that contains the button that called this function. The row is added
+    before the button. This function will only add row's to a div using 
+    css grid with two collumns; otherwise, two unformatted input cells
+    will be added.
+    Last Edit: 7/31/2021
+*/
+function addTwoInputRow() {
+    let parentDiv = this.parentElement;
+    let button = this;
+    let newInputName = document.createElement('input');
+    let newInputRatio = document.createElement('input');
+    newInputName.type = 'text';
+    newInputRatio.type = 'number';
+    newInputName.classList.add('name-input');
+    newInputRatio.classList.add('ratio-input');
+
+    parentDiv.insertBefore(newInputName, button);
+    parentDiv.insertBefore(newInputRatio, button);
+}
+
+//Event listener adding
+document.querySelectorAll('#item-secondary-unit-add-button, #item-ingredient-add-button').forEach( (button) => { 
+    button.addEventListener('click', addTwoInputRow); 
+});
+
+/*  Creation Date: 
+    Author: Ian Lubkin
+    Purpose:
+
+    Last Edit:
+*/
